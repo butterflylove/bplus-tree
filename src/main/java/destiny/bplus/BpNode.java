0143cd9b226c51c3b10160cd78da3b55aedb0eca
zhangtianlong01 <zhangtianlong01@baidu.com>
2017-10-16 14:33:28 +0800

    find

--- src/main/java/destiny/bplus/BpNode.java
+++ src/main/java/destiny/bplus/BpNode.java
@@ -1,5 +1,8 @@
 package destiny.bplus;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * B+树节点
  * @author zhangtianlong
@@ -31,6 +34,55 @@ public class BpNode {
      */
     BpNode next;
 
+
+    /**
+     * 节点的关键字列表
+     */
+    List<Tuple> entries;
+
+    /**
+     * 节点的指针列表
+     */
+    List<BpNode> children;
+
+    public BpNode(boolean isLeaf) {
+        this.isLeaf = isLeaf;
+        if (!isLeaf) {
+            children = new ArrayList<BpNode>();
+        }
+        entries = new ArrayList<Tuple>();
+    }
+
+    public BpNode(boolean isLeaf, boolean isRoot) {
+        this(isLeaf);
+        this.isRoot = isRoot;
+    }
+
+    public Tuple get(Tuple key) {
+        if (isLeaf) {
+            for (Tuple tuple : entries) {
+                if (key.compare(tuple) == 0) {
+                    return tuple;
+                }
+            }
+            return null;
+        } else {
+            // 小于首节点
+            if (key.compare(entries.get(0)) < 0) {
+                return children.get(0).get(key);
+            } else if (key.compare(entries.get(entries.size() - 1)) >= 0) {
+                return children.get(children.size() - 1).get(key);
+            } else {
+                // TODO 后续改为二分查找
+                for (int i = 0; i < (entries.size() - 1); i++) {
+                    if (key.compare(entries.get(i)) >= 0 && key.compare(entries.get(i + 1)) < 0) {
+                        return children.get(i + 1).get(key);
+                    }
+                }
+            }
+        }
+        return null;
+    }
 
 
 }
