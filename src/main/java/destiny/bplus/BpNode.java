b0b2ad58aadb037fda38b4854bc6810eb68c3cba
tianlong.ztl <payne.zhang@aihuishou.com>
2017-10-16 23:17:58 +0800

    parent insert

--- src/main/java/destiny/bplus/BpNode.java
+++ src/main/java/destiny/bplus/BpNode.java
@@ -46,9 +46,17 @@ public class BpNode {
     List<BpNode> children;
 
     /**
-     * 节点关键字的最大值
+     * 节点指针的最大值
      */
-    int maxLength = 16;
+    int maxLength = 17;
+
+    public BpNode getParent() {
+        return parent;
+    }
+
+    public void setParent(BpNode parent) {
+        this.parent = parent;
+    }
 
     public BpNode(boolean isLeaf) {
         this.isLeaf = isLeaf;
@@ -116,9 +124,110 @@ public class BpNode {
                 next = null;
                 //插入后再分裂
                 insertInLeaf(key);
-                int leftSize = entries.size() / 2;
-                int rightSize = entries.size() - leftSize;
 
+                int remainder = entries.size() % 2;
+                int leftSize;
+                if (remainder == 0) {
+                    leftSize = entries.size() / 2;
+                } else {
+                    leftSize = entries.size() / 2 + 1;
+                }
+                int rightSize = entries.size() - leftSize;
+                // 左右节点拷贝
+                for (int i = 0; i < leftSize; i++) {
+                    left.entries.add(entries.get(i));
+                }
+                for (int i = 0; i < rightSize; i++) {
+                    right.entries.add(entries.get(leftSize + i));
+                }
+                // 不是根节点
+                if (parent != null) {
+                    // 调整父子节点关系
+                    // 寻找当前节点在父节点的位置
+                    int index = parent.children.indexOf(this);
+                    // 删除当前指针
+                    parent.children.remove(index);
+                    left.setParent(parent);
+                    right.setParent(parent);
+                    // 将分裂后节点的指针添加到父节点
+                    parent.children.add(index, left);
+                    parent.children.add(index + 1, right);
+                    // for GC
+                    entries = null;
+                    children = null;
+
+                    // 父节点[非叶子节点]中插入关键字
+                    parent.insertInParent(right.entries.get(0));
+                    parent.updateNode(tree);
+                    // for GC
+                    parent = null;
+                }
+            }
+        }
+    }
+
+    /**
+     * 父节点插入关键字后,检查是否需要分裂
+     */
+    private void updateNode(BpTree tree) {
+        // 需要分裂
+        if (isNodeToSplit()) {
+            BpNode left = new BpNode(false);
+            BpNode right = new BpNode(false);
+            int remainder = entries.size() % 2;
+            int pLeftSize;
+            if (remainder == 0) {
+                pLeftSize = entries.size() / 2;
+            } else {
+                pLeftSize = entries.size() / 2 + 1;
+            }
+            int pRightSize = entries.size() - pLeftSize;
+            // 复制左边的关键字
+            for (int i = 0; i < (pLeftSize - 1); i++) {
+                left.entries.add(entries.get(i));
+            }
+            // 复制左边的指针
+            for (int i = 0; i < pLeftSize; i++) {
+                left.children.add(children.get(i));
+                children.get(i).setParent(left);
+            }
+
+            // 复制右边关键字,右边的第一个关键字提升到父节点
+            for (int i = 1;  i < pRightSize; i++) {
+                right.entries.add(entries.get(pLeftSize + i));
+            }
+            // 复制右边的指针
+            for (int i = 0; i < pRightSize; i++) {
+                right.children.add(children.get(pLeftSize + i));
+                children.get(i).setParent(right);
+            }
+            Tuple keyToParent = entries.get(pLeftSize);
+            if (parent != null) {
+                int index = parent.children.indexOf(this);
+                parent.children.remove(index);
+                left.parent = parent;
+                right.parent = parent;
+                parent.children.add(index, left);
+                parent.children.add(index + 1, right);
+                // 插入关键字
+                parent.insertInParent(keyToParent);
+                parent.updateNode(tree);
+                entries = null;
+                children = null;
+                parent = null;
+            } else {
+                // 是根节点
+                isRoot = false;
+                BpNode rootNode = new BpNode(false, true);
+                tree.root = rootNode;
+                left.parent = rootNode;
+                right.parent = rootNode;
+                rootNode.children.add(left);
+                rootNode.children.add(right);
+                children = null;
+                entries = null;
+                // 插入关键字
+                rootNode.insertInParent(keyToParent);
             }
         }
     }
@@ -128,7 +237,7 @@ public class BpNode {
      */
     private boolean isLeafToSplit() {
         if (isLeaf) {
-            if (entries.size() >= maxLength) {
+            if (children.size() >= maxLength) {
                 return true;
             }
             return false;
@@ -138,13 +247,42 @@ public class BpNode {
     }
 
     /**
-     * 插入到当前叶子节点中
+     * 中间节点是否需要分裂
+     */
+    private boolean isNodeToSplit() {
+        // 由于是先插入关键字,所以不需要[=]
+        if (isLeaf) {
+            throw new UnsupportedOperationException("error access to leaf");
+        }
+        if (children.size() > maxLength) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 插入到当前叶子节点中,不分裂
      */
     private void insertInLeaf(Tuple key) {
-        if (entries.size() == 0) {
-            entries.add(key);
-            return;
+        if (!isLeaf) {
+            throw new UnsupportedOperationException("can't insert into middle node.");
         }
+        insertImpl(key);
+    }
+
+    /**
+     * 插入到非叶子节点中,不分裂
+     */
+    private void insertInParent(Tuple key) {
+        if (isLeaf) {
+            throw new UnsupportedOperationException("can't insert into leaf node.");
+        }
+        insertImpl(key);
+    }
+
+
+    private void insertImpl(Tuple key) {
         //遍历插入
         for (int i = 0; i < entries.size(); i++) {
             if (entries.get(i).compare(key) == 0) {
